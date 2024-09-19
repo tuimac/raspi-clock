406d8565cbe94be8263bd7ae68364aed432860c9
tuimac <tuimac.devadm01@gmail.com>
2024-09-19 21:22:40 +0900

    create for self host github runner

---
+++ deploy/install-github-runner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GITHUB_REPO_NAME='raspi-clock'
+RUNNER_SCRIPT_DIR='/opt/github-runners/'${GITHUB_REPO_NAME}
+SERVICE_NAME='github-runner_'${GITHUB_REPO_NAME}'.service'
+
+[[ $USER -ne 'root' ]] && { echo 'Must be root!!'; exit 1; }
+
+cat << EOF >> /usr/lib/systemd/user/${SERVICE_NAME}
+[Unit]
+Description=Github Ruuner for $GITHUB_REPO_NAME
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=$RUNNER_SCRIPT_DIR/run.sh
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable $SERVICE_NAME
+systemctl start $SERVICE_NAME
