5fa19a36a6d11d8eadac5212bb9e36793cf643a6
tuimac <tuimac.devadm01@gmail.com>
2024-09-19 22:27:01 +0900

    add exit button

--- src/frontend/src/components/main/Main.js
+++ src/frontend/src/components/main/Main.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {
-  Box, Button, Divider, Grid, Typography, Stack
+  Box, Button, Divider, Grid, Typography, Stack,
+  IconButton
 } from '@mui/material';
 import { FullScreen } from 'react-full-screen';
 import { DAY_LIST, RESOLUTION } from '../../config/environment';
@@ -12,6 +13,7 @@ import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import WbCloudyIcon from '@mui/icons-material/WbCloudy';
 import { WiRainMix } from "react-icons/wi";
 import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 function Main({ fullScreenHandle }) {
 
@@ -28,56 +30,71 @@ function Main({ fullScreenHandle }) {
     }
   }
 
-  const getConfig = async () => {
-    return await ConfigService.getConfig().then((config_data) => {
-      setConfig(config_data);
-      return config_data;
-    });
-  }
-
-  const getNatureRemoDeviceInfo = async () => {
-    await NatureRemoServices.getNatureRemoDeviceInfo(config.token.natureremo).then((deviceInfo) => {
-      setDeviceInfo(deviceInfo);
-    });
-  }
-
-  const getClimateInfo = async () => {
-    await ClimateService.getClimate().then((climateInfo) => {
-      setClimateInfo(climateInfo['Feature'][0]['Property']['WeatherList']['Weather'])
-    });
-  }
-  
   const [config, setConfig] = useState({});
+
+  useEffect(() => {
+    configRef.current = config;
+  }, [config]);
+
+  const getConfig = () => {
+    ConfigService.getConfig().then((data) => {
+      setConfig(data)
+    });
+  }
+
+  const getNatureRemoDeviceInfo = () => {
+    // NatureRemoServices.getNatureRemoDeviceInfo(configRef.current.token.natureremo).then((deviceInfo) => {
+    //   setDeviceInfo(deviceInfo);
+    // });
+  }
+
+  const getClimateInfo = () => {
+    // ClimateService.getClimate().then((climateInfo) => {
+    //   setClimateInfo(climateInfo['Feature'][0]['Property']['WeatherList']['Weather'])
+    // });
+  }
+
   const [now, setNow] = useState(() => getNow());
   const [deviceInfo, setDeviceInfo] = useState('');
   const [climateInfo, setClimateInfo] = useState('');
+  const configRef = useRef(config);
   const env_icon_size = 38
   const env_gap = 2
   const divider_width = 8
 
   useEffect(() => {
-    getConfig().then((local_config) => {
-      getNatureRemoDeviceInfo(local_config.token.natureremo);
+    getConfig();
+    // Initial batch
+    const initialTimeout = setTimeout(() => {
+      getNatureRemoDeviceInfo();
       getClimateInfo();
-    });
-    setInterval(() => {
+    }, 1000);
+    // Polling batch
+    const intervalNow = setInterval(() => {
       setNow(() => getNow());
     }, 1000);
-    setInterval(() => {
+    const intervalClimate = setInterval(() => {
       getClimateInfo();
     }, 600000);
-    setInterval(() => {
+    const intervalNatureremo = setInterval(() => {
       getNatureRemoDeviceInfo();
     }, 60000);
+    // Process after unmount
+    return () => {
+      clearTimeout(initialTimeout);
+      clearInterval(intervalNow);
+      clearInterval(intervalClimate);
+      clearInterval(intervalNatureremo);
+    };
   }, []);
 
   return(
     <>
       <FullScreen handle={ fullScreenHandle }>
-        <Grid container direction='column' alignItems='center' justifyContent='center'>
-          <Box sx={{ display: 'flex', height: RESOLUTION.height, width: RESOLUTION.width }}>
-            <Grid container item direction='column' alignItems='center' justifyContent='flex-start' spacing={2}>
-              <Grid container item direction='row' alignItems='center' justifyContent='space-around' spacing={2}>
+        <Grid container direction='column' alignItems='center' justifyContent='center' spacing={0}>
+          <Box sx={{ display: 'flex', height: RESOLUTION.height, width: RESOLUTION.width, border: 1 }}>
+            <Grid container item direction='column' alignItems='center' justifyContent='flex-start' spacing={0}>
+              <Grid container item direction='row' alignItems='center' justifyContent='space-around' spacing={0}>
                 <Grid item>
                   <Stack alignItems='center' justifyContent='flex-start' direction='row' gap={4}>
                     <Typography variant='h4'>{ `${now.year}/${now.month}/${now.day}` }</Typography>
@@ -90,34 +107,37 @@ function Main({ fullScreenHandle }) {
                 </Grid>
                 <Divider flexItem sx={{ borderRightWidth: divider_width }} orientation='vertical'/>
                 <Grid item>
-                  <Button
-                    size="medium"
-                    variant="outlined"
-                    color="success"
-                    onClick={ fullScreenHandle.exit }
-                  >Escape</Button>
-                  {
-                    deviceInfo === ''
-                    ? <Stack alignItems='center' direction='row' gap={env_gap}>
-                        <DeviceThermostatIcon style={{ fontSize: env_icon_size }}/>
-                        <Typography variant='h4'>- ℃</Typography>
-                      </Stack>
-                    : <Stack alignItems='center' direction='row' gap={env_gap}>
-                        <DeviceThermostatIcon style={{ fontSize: env_icon_size }}/>
-                        <Typography variant='h4'>{ deviceInfo.te.val }℃</Typography>
-                      </Stack>
-                  }
-                  {
-                    deviceInfo === ''
-                    ? <Stack alignItems='center' direction='row' gap={env_gap}>
-                        <WaterDropIcon style={{ fontSize: env_icon_size }}/>
-                        <Typography variant='h4'>- %</Typography>
-                      </Stack>
-                    : <Stack alignItems='center' direction='row' gap={env_gap}>
-                        <WaterDropIcon style={{ fontSize: env_icon_size }}/>
-                        <Typography variant='h4'>{ deviceInfo.hu.val }%</Typography>
-                      </Stack>
-                  }
+                  <Stack alignItems='flex-start' justifyContent='flex-start' direction='column' gap={3}>
+                    <Stack justifyContent='end' direction='row'>
+                      <IconButton color='white' onClick={ fullScreenHandle.exit }>
+                        <ExitToAppIcon style={{ width: 50, height: 50, padding: 0 }}/>
+                      </IconButton>
+                    </Stack>
+                    <Stack alignItems='flex-start' justifyContent='center' direction='column'>
+                      {
+                        deviceInfo === ''
+                        ? <Stack alignItems='center' direction='row' gap={env_gap}>
+                            <DeviceThermostatIcon style={{ fontSize: env_icon_size }}/>
+                            <Typography variant='h4'>- ℃</Typography>
+                          </Stack>
+                        : <Stack alignItems='center' direction='row' gap={env_gap}>
+                            <DeviceThermostatIcon style={{ fontSize: env_icon_size }}/>
+                            <Typography variant='h4'>{ deviceInfo.te.val }℃</Typography>
+                          </Stack>
+                      }
+                      {
+                        deviceInfo === ''
+                        ? <Stack alignItems='center' direction='row' gap={env_gap}>
+                            <WaterDropIcon style={{ fontSize: env_icon_size }}/>
+                            <Typography variant='h4'>- %</Typography>
+                          </Stack>
+                        : <Stack alignItems='center' direction='row' gap={env_gap}>
+                            <WaterDropIcon style={{ fontSize: env_icon_size }}/>
+                            <Typography variant='h4'>{ deviceInfo.hu.val }%</Typography>
+                          </Stack>
+                      }
+                    </Stack>
+                  </Stack>
                 </Grid>
               </Grid>
               <Divider flexItem sx={{ borderBottomWidth: divider_width }}/>
